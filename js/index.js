48676d9ea883d0d88ad0d525ce028dc19b04fd3c
Raadwan Masum <piraadwan@gmail.com>
2020-08-15 13:44:17 -0700

    create render functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
 console.log("Team Optix 3749");
+
+document.getElementById("software")
+document.getElementById("electrical")
+document.getElementById("design")
+document.getElementById("build")
+
+const renderGeneral = () => {
+    document.getElementById("general").style.display = "inline-block"
+    document.getElementById("software").style.display = "none"
+    document.getElementById("electrical").style.display = "none"
+    document.getElementById("design").style.display = "none"
+    document.getElementById("build").style.display = "none"
+}
+
+const renderSoftware = () => {
+    document.getElementById("software").style.display = "inline-block"
+    document.getElementById("general").style.display = "none"
+    document.getElementById("electrical").style.display = "none"
+    document.getElementById("design").style.display = "none"
+    document.getElementById("build").style.display = "none"
+}
+
+const renderElectrical = () => {
+    document.getElementById("electrical").style.display = "inline-block"
+    document.getElementById("general").style.display = "none"
+    document.getElementById("software").style.display = "none"
+    document.getElementById("design").style.display = "none"
+    document.getElementById("build").style.display = "none"
+}
+
+const renderDesign = () => {
+    document.getElementById("design").style.display = "inline-block"
+    document.getElementById("general").style.display = "none"
+    document.getElementById("software").style.display = "none"
+    document.getElementById("electrical").style.display = "none"
+    document.getElementById("build").style.display = "none"
+}
+
+const renderBuild = () => {
+    document.getElementById("build").style.display = "inline-block"
+    document.getElementById("general").style.display = "none"
+    document.getElementById("software").style.display = "none"
+    document.getElementById("electrical").style.display = "none"
+    document.getElementById("design").style.display = "none"
+}
